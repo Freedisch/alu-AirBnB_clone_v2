16e5b58fd6cd6d959200e16c13c1bb833d89a577
martin ndungu <M.ndungu@alustudent.com>
2023-01-31 20:57:18 +0300

    cls

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 ''' new class filestorage that stores new objects in a json file''' 
 
 
-from models import base_model
+
 import json
 
 ''' class filestorage that will serialize instances of an object into a Json file. 
@@ -41,12 +41,13 @@ class FileStorage:
         
         '''deserializes the json file (__file_path) to t
         the __objects dictionary''' 
+        from models.base_model import BaseModel
         try:
             with open(self.__file_path, 'r') as f: 
                 serial_dictionary = json.load(f)
-                for value in serial_dictionary.values():
-                    cls_name = value['__class__']
-                    self.new(eval(cls_name)(str(value)))
+                for item in serial_dictionary.values():
+                    cls_name = item['__class__']
+                    self.new(eval(cls_name + "(**" + str(item) + ")"))
                    
         except FileNotFoundError():
             pass
