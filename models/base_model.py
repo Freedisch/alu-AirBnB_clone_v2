be4d2b84bee595b21c4e8eea1979430f6b80ab36
martin ndungu <M.ndungu@alustudent.com>
2023-01-31 16:13:57 +0300

    change from read

--- models/base_model.py
+++ models/base_model.py
@@ -49,19 +49,3 @@ class BaseModel:
         dictionary["__class__"] = name
         return self.__dict__
 
-
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
-
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
-
-model2 = storage.reload()
-print(model2)
