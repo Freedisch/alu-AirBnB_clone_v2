7117eeb06bfa20c7d9970fecf6a25f758119f79f
martin ndungu <M.ndungu@alustudent.com>
2023-02-03 16:12:52 +0300

    change tform

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize new BaseModel."""
 
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        tform = "%Y-%m-%d"
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
